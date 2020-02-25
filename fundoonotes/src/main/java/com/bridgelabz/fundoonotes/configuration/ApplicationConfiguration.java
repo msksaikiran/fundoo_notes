22846657a5a5fcb615834c5d3d654b514f7ca07e
msksaikiran <msaikiran0728@gmail.com>
2020-02-26 00:56:29 +0530

    [Sai] refractor. renames changed the package name

--- fundoonotes/src/main/java/com/bridgelabz/fundoonotes/configuration/ApplicationConfiguration.java
+++ fundoonotes/src/main/java/com/bridgelabz/fundoonotes/configuration/ApplicationConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class ConfigurFile {
+public class ApplicationConfiguration {
 	  
     @Bean
     public  PasswordEncoder passwordEncoder() {
