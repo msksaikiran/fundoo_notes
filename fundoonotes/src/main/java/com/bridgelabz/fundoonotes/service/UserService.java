ad682ccd0b06b868540b44fec6ffc62927ab19f2
msksaikiran <msaikiran0728@gmail.com>
2020-02-26 22:06:00 +0530

    [Sai] refractor. added the api for updating the password

--- fundoonotes/src/main/java/com/bridgelabz/fundoonotes/service/UserService.java
+++ fundoonotes/src/main/java/com/bridgelabz/fundoonotes/service/UserService.java
@@ -2,19 +2,24 @@ package com.bridgelabz.fundoonotes.service;
 
 import java.util.List;
 
+import com.bridgelabz.fundoonotes.dto.Login;
+import com.bridgelabz.fundoonotes.dto.Register;
+import com.bridgelabz.fundoonotes.dto.Update;
 import com.bridgelabz.fundoonotes.entity.User;
 
 public interface UserService {
 
-	public User login(int id, User userRecord);
+	public User login(int id, Login userRecord);
 	
-	public void register(User user);
+	public User register(Register userRecord);
 	
-	public void forgotPassword(String email, String password);
+	public User forgotPassword(String token,Update user);
 	
 	public List<User> getUsers(User userRecord);
 	
 	public void removeUser(User userRecord, String id);
+
+	Boolean verify(String token);
 
 	
 
