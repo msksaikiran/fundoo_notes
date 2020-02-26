d40ad7ca2fdf2349e49e8e56db5b9aca25956ead
msksaikiran <msaikiran0728@gmail.com>
2020-02-26 22:00:47 +0530

    [Sai] refractor. handling the proper exception

--- fundoonotes/src/main/java/com/bridgelabz/fundoonotes/exception/CustomisedResponseEntityExceptionHandler.java
+++ fundoonotes/src/main/java/com/bridgelabz/fundoonotes/exception/CustomisedResponseEntityExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.bridgelabz.fundoonotes.exception;
 
 import java.util.Date;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,16 +8,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.bridgelabz.fundoonotes.entity.ExceptionResponse;
+import com.bridgelabz.fundoonotes.response.ExceptionResponse;
+
 
 @ControllerAdvice
 public class CustomisedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
+	
+	
 	@ExceptionHandler(UserNotFoundException.class)
-	public final ResponseEntity<Object> handlerUserNotFoundException(UserNotFoundException ex, WebRequest request) {
+	public final ResponseEntity<ExceptionResponse> handlerUserNotFoundException(UserNotFoundException ex, WebRequest request) {
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
 				request.getDescription(false));
-		return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+		return new ResponseEntity(HttpStatus.NOT_FOUND);
 	}
 
 }
