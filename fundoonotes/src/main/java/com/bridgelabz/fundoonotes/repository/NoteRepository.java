2fd6e24d7654a99f2ebf5e6f04013a4dbb9a91c9
msksaikiran <msaikiran0728@gmail.com>
2020-02-26 22:02:41 +0530

    [Sai] refractor. added the updated query

--- fundoonotes/src/main/java/com/bridgelabz/fundoonotes/repository/NoteRepository.java
+++ fundoonotes/src/main/java/com/bridgelabz/fundoonotes/repository/NoteRepository.java
@@ -1,11 +1,17 @@
 package com.bridgelabz.fundoonotes.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.bridgelabz.fundoonotes.entity.Noteinfo;
 
 
-public interface NoteRepository extends CrudRepository<Noteinfo, String> 
-{  
+public interface NoteRepository extends CrudRepository<Noteinfo, Integer> 
+{
+
+	@Query(value = "select * from fundoosai.noteinfo where user_id=?", nativeQuery = true)
+	List<Noteinfo> findAllById(int noteId);  
 
 }  
--- fundoonotes/src/main/java/com/bridgelabz/fundoonotes/repository/UserRepository.java
+++ fundoonotes/src/main/java/com/bridgelabz/fundoonotes/repository/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.bridgelabz.fundoonotes.entity.User;
 
-public interface UserRepository extends CrudRepository<User, String> {
+public interface UserRepository extends CrudRepository<User, Integer> {
 	@Query(value="select * from fundoosai.user where id=?",nativeQuery=true)
 	public String login();
 	
@@ -15,11 +15,15 @@ public interface UserRepository extends CrudRepository<User, String> {
     public User register(User user);
 	
 	@Query(value = "update user set password=? where email=?", nativeQuery = true)
-	public User forgotPassword(String password,String email);
+	public User forgotPassword(User user);
 	
 	@Query(value = "select * from fundoosai.user where id=?", nativeQuery = true)
 	public User getUserById(int id);
+	
+	@Query(value="select * from fundoosai.user where email=?",nativeQuery=true)
+	public User getUserByEmail(String email);
 
-	public List<User> getUserByName(int id);
+	@Query(value="update fundoosai.user set is_verified=true where id=?",nativeQuery=true)
+	public void verify(Integer id);
 
 }
