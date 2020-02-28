6e85f616adfd9001444e699207af06b04e6c35d1
msksaikiran <msaikiran0728@gmail.com>
2020-02-28 10:09:16 +0530

    [Sai] add. entity classess in order to communicate with the database

---
+++ fundoo_note_api/src/main/java/com/bridgelabz/fundoo_note_api/entity/Noteinfo.java
@@ -0,0 +1,129 @@
+package com.bridgelabz.fundoo_note_api.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Noteinfo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long noteId;
+
+	private String title;
+
+	private String description;
+
+	private int isArchieved;
+
+	private int isPinned;
+
+	private int isTrashed;
+
+	private LocalDateTime createdDateAndTime;
+
+	private LocalDateTime upDateAndTime;
+
+	private String colour;
+
+	private LocalDateTime reminder;
+
+	@ManyToOne(cascade = CascadeType.ALL)
+    private User user;
+
+	public Long getNoteId() {
+		return noteId;
+	}
+
+	public void setNoteId(Long noteId) {
+		this.noteId = noteId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getIsArchieved() {
+		return isArchieved;
+	}
+
+	public void setIsArchieved(int isArchieved) {
+		this.isArchieved = isArchieved;
+	}
+
+	public int getIsPinned() {
+		return isPinned;
+	}
+
+	public void setIsPinned(int isPinned) {
+		this.isPinned = isPinned;
+	}
+
+	public int getIsTrashed() {
+		return isTrashed;
+	}
+
+	public void setIsTrashed(int isTrashed) {
+		this.isTrashed = isTrashed;
+	}
+
+	public LocalDateTime getCreatedDateAndTime() {
+		return createdDateAndTime;
+	}
+
+	public void setCreatedDateAndTime(LocalDateTime createdDateAndTime) {
+		this.createdDateAndTime = createdDateAndTime;
+	}
+
+	public LocalDateTime getUpDateAndTime() {
+		return upDateAndTime;
+	}
+
+	public void setUpDateAndTime(LocalDateTime upDateAndTime) {
+		this.upDateAndTime = upDateAndTime;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	public LocalDateTime getReminder() {
+		return reminder;
+	}
+
+	public void setReminder(LocalDateTime reminder) {
+		this.reminder = reminder;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+}
