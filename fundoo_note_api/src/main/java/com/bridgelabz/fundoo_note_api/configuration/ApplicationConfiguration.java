cc0f283df03413d2acdc4fd86a896096ff4da101
msksaikiran <msaikiran0728@gmail.com>
2020-02-29 10:15:24 +0530

    [Sai] refractor. removed the warnings

--- fundoo_note_api/src/main/java/com/bridgelabz/fundoo_note_api/configuration/ApplicationConfiguration.java
+++ fundoo_note_api/src/main/java/com/bridgelabz/fundoo_note_api/configuration/ApplicationConfiguration.java
@@ -2,7 +2,6 @@ package com.bridgelabz.fundoo_note_api.configuration;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
