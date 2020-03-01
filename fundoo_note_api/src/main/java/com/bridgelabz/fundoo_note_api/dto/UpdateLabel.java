bbfe21baa7716335b76d2de976acda8dcd760525
msksaikiran <msaikiran0728@gmail.com>
2020-03-01 18:34:54 +0530

    [Sai] refractor. changed the dataMembers

---
+++ fundoo_note_api/src/main/java/com/bridgelabz/fundoo_note_api/dto/UpdateLabel.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.fundoo_note_api.dto;
+
+public class UpdateLabel {
+
+	private String lName;
+
+	public String getlName() {
+		return lName;
+	}
+
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+
+
+}
